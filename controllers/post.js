04377e75cef187987bd2e02a195de3097d059a36
mettamage <mettamage@users.noreply.github.com>
2016-11-24 21:02:43 +0100

    create, delete, edit post are working now including using sessions

--- controllers/post.js
+++ controllers/post.js
@@ -26,8 +26,47 @@ ANATOMY OF A POST (_id is automatically generated)
 
 router.route('/admin')
 	.get( (request, response) => {
-		response.render('admin', {username: request.session.username, password: request.session.password})
+		let posts = model.getAllPosts();
+		posts.then( (posts) => {
+			console.log('session...\n');
+			console.log(request.session);
+			if(request.session.isEdited){
+				const body = request.session.postBody;
+				const title = request.session.postTitle;
+				request.session.postBody = '';
+				request.session.postTitle = '';
+				request.session.isEdited = false;
+			response.render('admin', 
+				{
+				action: '/admin/' + title + '/edit',
+				title: title,
+				body: body,
+				posts: posts, 
+				username: request.session.username, 
+				password: request.session.password
+				});
+				console.log('get1 ' + request.session.isEdited);
+			}
+			else{
+			response.render('admin', 
+				{
+				action: '/admin/createPost',
+				posts: posts, 
+				username: request.session.username, 
+				password: request.session.password
+				});
+				console.log('get2 ' + request.session.isEdited);
+			}
+		});
 	});
+
+// router.route('/admin')
+// 	.get( (request, response) => {
+// 		let posts = model.getAllPosts();
+// 		posts.then( (posts) => {
+// 			console.log(posts);
+// 		});
+// 	});
 
 router.route('/admin/createPost')
 	.post( (request, response) => {
@@ -43,7 +82,7 @@ router.route('/admin/createPost')
 				request.session.username,  
 				post.title, 
 				post.body);
-			createdPost.then( x => response.redirect('/admin'));
+			createdPost.then( () => response.redirect('/admin'));
 		}
 		else{
 			reportError(response, keys);
@@ -69,28 +108,50 @@ router.route('/:postTitle')
 	});
 
 
-
+//deletes *all* posts with the same title, since the key should be unique (not implemented yet)
 router.route('/admin/:postTitle/delete')
 	.get( (request, response) => {
 		let postTitle = model.deletePostByTitle(request.params.postTitle);
 		postTitle.then( (result) => {
-			response.send(result);
+			console.log('post deleted\n');
+			console.log(result);
+			response.redirect('/admin');
 		});
 	});
 
 router.route('/admin/:postTitle/edit')
+	.get( (request, response) => {
+		console.log('request.params.title\n');
+		console.log(request.params.title);
+		let postTitle = model.getPostByTitle(request.query.title);
+		//Waarom wordt result als een array teruggegeven?
+		//Geeft de onderliggende implementatie het zo terug? This has to change...
+		postTitle.then( (result) => {
+			console.log('postTitle');
+			console.log(result);
+			console.log(result[0].body);
+			console.log(result[0].title);
+			request.session.postBody = result[0].body;
+			request.session.postTitle = result[0].title;
+			request.session.isEdited = true;
+			response.redirect('/admin');
+		})
+		.catch( (error) => {
+			console.log(error);
+		});
+	})
 	.post( (request, response) => {
 		const post = request.body;
 		const keys = Object.keys(post);
 		const vals = keys.map(key => post[key]);
-		console.log(keys);
 		const hasAllProperties = checkProperties(post, 'title', 'body');
 		if(hasAllProperties){
 			let editedPost = model.editPostByTitle(request.params.postTitle, post.title, post.body);
 			editedPost.then( (result) => {
-				response.send(result);
+				console.log('post edited\n');
+				console.log(result);
+				response.redirect('/admin');
 			});
-			// .then( x => response.redirect('/'));
 		}
 		else{
 			reportError(response, keys);
